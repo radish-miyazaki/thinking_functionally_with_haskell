9a7d6aaa1f38d11e6ca0dda4e8ad5e10bd8c5e3a
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-07-08 21:45:33 +0900

    finished Sec2.7

--- section2.hs
+++ section2.hs
@@ -24,3 +24,11 @@ cwords n infile outfile
          ; writeFile outfile (commonWords n text)
          ; putStrLn "cwords done!"
          }
+
+roots :: (Float, Float, Float) -> (Float, Float)
+roots (a, b, c)
+    | a == 0 = error "not quadratic"
+    | disc < 0 = error "complex roots"
+    | otherwise = ((-b + r) / e, (-b - r) / e)
+    where
+        { disc = b * b - 4 * a * c; r = sqrt disc; e = 2 * a }
